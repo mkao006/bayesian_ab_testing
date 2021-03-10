44c3e520114b5571218580e093f13d15f6096b01
Michael C. J. Kao <mkao006@gmail.com>
2021-03-10 23:08:00 +1100

    added method to summarise the traffic

--- src/utils/simulator.py
+++ src/utils/simulator.py
@@ -1,26 +1,30 @@
 ''' Simulate traffic for AB testing
 '''
 import torch
-import pyro
 import pyro.distributions as dist
+
 
 class ProportionTrafficGenerator():
     def __init__(self,
                  N: torch.tensor,
                  p: torch.tensor,
-                 traffic_size: torch.tensor,                 
+                 traffic_size: torch.tensor,
                  treatment_proportion: torch.tensor):
         self.N = N
-        self.p = p        
+        self.p = p
         self.traffic_size = traffic_size
         self.treatment_proportion = treatment_proportion
         self.control_size = (self.traffic_size * (1 - self.treatment_proportion)).floor()
         self.variant_size = self.traffic_size - self.control_size
         self.split_traffic_size = torch.stack([self.control_size, self.variant_size])
 
-    def generate_traffic(self, normalise: bool=False) -> torch.tensor:
-        if normalise:
-            return dist.Binomial(self.split_traffic_size, self.p).sample((self.N, ))/\
-                self.split_traffic_size
-        else:
-            return dist.Binomial(self.split_traffic_size, self.p).sample((self.N, ))
+    def generate_traffic(self) -> torch.tensor:
+        self.sample = dist.Binomial(self.split_traffic_size, self.p).sample((self.N, ))
+        self.summarise_traffic()
+
+    def summarise_traffic(self):
+        self.x0 = self.sample[:, 0].sum()
+        self.n0 = self.split_traffic_size[0] * self.N
+
+        self.x1 = self.sample[:, 1].sum()
+        self.n1 = self.split_traffic_size[1] * self.N
